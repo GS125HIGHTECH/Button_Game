7e9f79340c7a5e858b385f93d451f5b95e6a15a5
gs125 <gs214gs@gmail.com>
2024-03-17 17:08:46 +0100

    Dodaj pliki projektów.

---
+++ Controllers/ButtonController.cs
@@ -0,0 +1,32 @@
+﻿using ButtonGrid.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ButtonGrid.Controllers
+{
+    public class ButtonController : Controller
+    {
+        static List<ButtonModel> buttons = new List<ButtonModel>();
+        readonly Random random = new Random();
+        const int GRID_SIZE = 25;
+        public IActionResult Index()
+        {
+            if(buttons.Count < GRID_SIZE)
+            {
+                for (int i = 0; i < GRID_SIZE; i++)
+                {
+                    buttons.Add(new ButtonModel { Id = i, ButtonState = random.Next(4) });
+                }
+            }
+           
+            return View("Index", buttons);
+        }
+
+        public IActionResult HandleButtonClick(string buttonNumber)
+        {
+            int Id = int.Parse(buttonNumber);
+            buttons.ElementAt(Id).ButtonState = (buttons.ElementAt(Id).ButtonState + 1) % 4;
+
+            return View("Index", buttons);
+        }
+    }
+}
---
+++ Models/ButtonModel.cs
@@ -0,0 +1,8 @@
+﻿namespace ButtonGrid.Models
+{
+    public class ButtonModel
+    {
+        public int Id { get; set; }
+        public int ButtonState { get; set; }
+    }
+}
